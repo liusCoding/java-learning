d6a2640c3e06f306d86ffb76f1872ec7d9c30fea
liusCoding <14786469221@163.com>
2020-04-24 11:25:26 +0800

    Validator + 自动引发异常来进行参数校验

--- validation-exception-handler/src/main/java/com/liuscoding/api/controller/UserController.java
+++ validation-exception-handler/src/main/java/com/liuscoding/api/controller/UserController.java
@@ -5,8 +5,6 @@ import com.liuscoding.api.service.IUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,11 +28,7 @@ public class UserController {
 
     @ApiOperation("添加用户")
     @PostMapping("/addUser")
-    public String addUser(@Valid  @RequestBody User user, BindingResult bindingResult){
-        //如果有参数校验失败，会将错误信息封装成对象在BingResult里
-        for (ObjectError error : bindingResult.getAllErrors()) {
-            return error.getDefaultMessage();
-        }
+    public String addUser(@Valid  @RequestBody User user){
         return userService.addUser(user);
     }
 
