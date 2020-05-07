caebb2c9157234df4356a6af5058971cfd9eed61
liusCoding <14786469221@163.com>
2020-05-07 11:12:57 +0800

    模拟同步和非同步同时运行

---
+++ juc-master/src/main/java/com/liuscoding/juc/t004/T.java
@@ -0,0 +1,40 @@
+package com.liuscoding.juc.t004;
+
+
+/**
+ * 同步和非同步方法是否可以同时调用
+ */
+public class T {
+
+    public synchronized void m1(){
+        System.out.println(Thread.currentThread().getName() + "m1 start....");
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Thread.currentThread().getName() + "m1 end....");
+
+    }
+
+
+    public void m2(){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Thread.currentThread().getName()+"m2");
+    }
+
+
+    public static void main(String[] args) {
+        T t = new T();
+
+        new Thread(t::m1,"m1:-----------").start();
+        new Thread(t::m2,"m2：-----------").start();
+    }
+}
